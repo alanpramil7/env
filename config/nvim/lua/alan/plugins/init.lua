c4e437e4ae3669dc959a0bce36db305a7e43c651
Alan <alanpramil7@gmail.com>
2025-05-10 16:14:05 +0530

    nvim plugins

--- config/nvim/lua/alan/plugins/init.lua
+++ config/nvim/lua/alan/plugins/init.lua
@@ -1,4 +1,31 @@
 return {
-    "nvim-lua/plenary.nvim",
-    name = "plenary"
+    {
+        "nvim-lua/plenary.nvim",
+        name = "plenary"
+    },
+    {
+        "kylechui/nvim-surround",
+        event = { "BufReadPre", "BufNewFile" },
+        version = "*", -- Use for stability; omit to use `main` branch for the latest features
+        config = true,
+    },
+    {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        opts = {},
+        -- stylua: ignore
+        keys = {
+            { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+            { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+            { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+            { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+        },
+    },
+    {
+
+        "lewis6991/gitsigns.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        config = true,
+    },
 }
--- config/nvim/lua/alan/plugins/telescope.lua
+++ config/nvim/lua/alan/plugins/telescope.lua
@@ -19,7 +19,6 @@ return {
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
         vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
         vim.keymap.set('n', '<leader>gc', builtin.git_commits, {})
-        vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
         vim.keymap.set('n', '<C-p>', builtin.lsp_document_symbols, {})
         vim.keymap.set('n', '<leader>hh', builtin.help_tags, {})
         vim.keymap.set('n', '<leader>/', function()
